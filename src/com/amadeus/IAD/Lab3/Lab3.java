59e98cd2ecae9eeb8c6c2af9de1cf8a6415f9c16
amadeus-tentatio <martinraila@mail.ru>
2016-10-09 21:44:14 +0300

    Added some classes

--- src/com/amadeus/IAD/Lab3/Lab3.java
+++ src/com/amadeus/IAD/Lab3/Lab3.java
@@ -1,6 +1,6 @@
 package com.amadeus.IAD.Lab3;
 
-public class Main {
+public class Lab3 {
 
     public static void main(String[] args) {
 	// write your code here
---
+++ src/com/amadeus/IAD/Lab3/Outline.java
@@ -0,0 +1,7 @@
+package com.amadeus.IAD.Lab3;
+
+/**
+ * Created by admin on 09.10.2016.
+ */
+public class Outline {
+}
